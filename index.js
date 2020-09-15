19c163dc2348374f5cdd9056ac3a3787c6216769
Pat  Pignataro <pat6284@msn.com>
2020-09-15 11:46:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,8 +5,94 @@ const fi = (function() {
     },
 
     each: function(collection, callback) {
-      
+      if (Array.isArray(collection)) {
+        collection.forEach(element => 
+          callback(element)
+        );
+      } else { // collection is an Object
+        for (const key in collection) {
+          if (collection.hasOwnProperty(key)) {
+            const element = collection[key];
+            callback(element)
+          }
+        }
+      }
+      return collection
+    },
 
+    map: function(collection, callback) {
+      let newCollection = []
+
+      if (Array.isArray(collection)) {
+        collection.forEach(element => 
+          newCollection.push(callback(element))
+        );
+      } else { // collection is an Object
+        for (const key in collection) {
+          if (collection.hasOwnProperty(key)) {
+            const element = collection[key];
+            newCollection.push(callback(element))
+          }
+        }
+      }
+      return newCollection
+    },
+
+    reduce: function(collection, callback, acc) {
+      let total = (!!acc) ? acc : collection[0]
+      let i = (!!acc) ? 0 : 1
+
+      for (; i < collection.length; i++) {
+        total = callback(total, collection[i], collection)
+      }
+      return total
+    },
+
+    find: function(collection, predicate) {
+      // if collection is an object, make values an array
+      if (!(Array.isArray(collection))) {
+        collection = Object.values(collection)
+      }
+      // iterate over collection array until predicate true
+      for (let i = 0; i < collection.length; i++) {
+        if (predicate(collection[i])) {
+          return collection[i]
+        }
+      }
+    },
+
+    filter: function(collection, predicate) {
+      let truthValues = []
+      if (!(Array.isArray(collection))) {
+        collection = Object.values(collection)
+      }
+      for (let i = 0; i < collection.length; i++) {
+        if (predicate(collection[i])) {
+          truthValues.push(collection[i])
+        }
+      }
+      return truthValues
+    },
+
+    size: function(collection) {
+    if (!(Array.isArray(collection))) {
+        collection = Object.values(collection)
+      }
+      return collection.length
+    },
+
+    first: function(array, num) {
+      let n = (!!num) ? num : 1
+      let nArray = array.slice(0, n)
+
+      return (!!num) ? nArray : nArray[0]
+    },
+
+    last: function(array, num) {
+      let n = (!!num) ? -num : -1
+      let nArray = array.slice(n)
+
+      return (!!num) ? nArray : nArray[0]
     },
 
     map: function() {
